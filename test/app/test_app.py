07b45be7f7f23e255ac2fa7bd1fd14ac8226c81f
32-Levels <augustwaller2@gmail.com>
2020-07-24 19:47:23 -0400

    AW_0: added some tests

---
+++ test/app/test_app.py
@@ -0,0 +1,57 @@
+import pytest
+from unittest.mock import MagicMock
+from app import data_access_service, config_service
+import app
+from praw.models import Message
+from praw.reddit import Reddit
+import datetime
+
+
+def test_is_too_old():
+    date = datetime.date.today()-config_service.PERIOD-1
+    assert app._is_too_old(date)
+    date = date + 2
+    assert app._is_too_old(date) == False
+
+
+def test_is_user_subscribed():
+    title = '[DISC] some title'
+    user = { 'mangoes' ['different title', 'some title']}
+    assert app._is_user_subscribed(user, title)
+    title = 'abcd'
+    assert app._is_user_subscribed(user, title) == False
+
+
+def test_update_users_subscriptions():
+    app._append_mangoes_to_message = MagicMock(return_value="# Manga Updates:\n\n")
+    app._format_and_send_message = MagicMock()
+    r = MagicMock()
+    assert app._update_users_subscriptions({}, r) == None
+    app._format_and_send_message.assert_not_called()
+
+    message = "# Manga Updates:\n\nUpdates HERE\n"
+    app._append_mangoes_to_message.return_value = message
+    assert app._update_users_subscriptions({}, r) == None
+    app._format_and_send_message.assert_called_once_with(message, r, {})
+
+
+def test_append_mangoes_to_message():
+    r = MagicMock()
+    r.subreddit.new.return_value = [{"title": "a", "link": "b", "created_utc": datetime.date.today()}, {"title": "c", "link": "d", "created_utc": datetime.date.today()-config_service.PERIOD-1}]
+    r._is_user_subscribed.return_value = False
+    message = "abc\n\n"
+    assert app._append_mangoes_to_message(message, r, {}) == message
+    
+    r._is_user_subscribed.return_value = True
+    assert app._append_mangoes_to_message(message, r, {}) == "abc\n\n[a](b)\n"
+
+
+def test_format_and_send_message():
+
+
+
+
+def test_update_subscriptions():
+    data_access_service.fetch_subscriber_data = MagicMock()
+    app._update_users_subscriptions = MagicMock()
+    
--- test/app/test_data_access_service.py
+++ test/app/test_data_access_service.py
@@ -4,7 +4,43 @@ from app import config_service, data_access_service, data_transform_util
 import importlib
 import os
 from praw import Reddit
+from praw.models import Message
 
 
 def test_fetch_new_subscriber_messages():
-    
+    r = MagicMock()
+    mess = Mressage()
+    r.inbox.unread.return_value = ['a', mess]
+    assert data_access_service._fetch_new_subscriber_messages(r) == [mess]
+    r.inbox.mark_read.assert_called_once_with(['a', mess])
+
+
+def test_fetch_from_so_called_db():
+    data_transform_util = MagicMock()
+    data_transform_util.init_db.return_value = 'result'
+    reddit = MagicMock()
+    reddit.inbox.sent.return_value = ['test']
+    assert data_access_service._fetch_from_so_called_db(reddit) == 'result'
+
+    reddit.inbox.sent.return_value = None
+    assert data_access_service._fetch_from_so_called_db(reddit) == []
+
+
+def test_fetch_and_update_so_called_db():
+    result = ['result']
+    data_access_service._fetch_from_so_called_db = MagicMock(return_value=result)
+    data_transform_util.update_and_format_to_writeable_db_data = MagicMock()
+    r = MagicMock()
+    redditor = MagicMock()
+    r.redditor.return_value = redditor
+    assert data_access_service._fetch_and_update_so_called_db(['x'], r) == result
+    data_access_service._fetch_from_so_called_db.assert_called_once()
+    data_transform_util.update_and_format_to_writeable_db_data.assert_called_once_with(result, ['x'])
+    r.redditor.assert_called_once_with(config_service.REDDIT_USER)
+    redditor.message.assert_called_once_with(config_service.DB_MESSAGE_SUBJECT, result)
+
+
+def test_fetch_subscriber_data():
+    data_access_service._fetch_new_subscriber_messages = MagicMock()
+    data_access_service._fetch_and_update_so_called_db = MagicMock(return_value='result')
+    assert data_access_service.fetch_subscriber_data(MagicMock())
